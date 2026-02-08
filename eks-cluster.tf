a345177fc94d1cea5cf8b3ad03576c7a632997c8
DianaBianca <dianabiancasr@gmail.com>
2026-02-08 16:06:27 -0300

    WIP: criação de infra

---
+++ eks-cluster.tf
@@ -0,0 +1,19 @@
+resource "aws_eks_cluster" "cluster" {
+  name = "eks-${var.projectName}-v1"
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = concat(
+      aws_subnet.subnet_public[*].id,
+      aws_subnet.subnet_private[*].id
+    )
+    security_group_ids = [aws_security_group.sg.id]
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources"
+  default     = "us-east-1"
+}
+
+
+variable "projectName" {
+  description = "The name of the project"
+  default     = "tc3-g38-lanchonete-v1"
+}
+
+variable "cidr_vpc" {
+  description = "The CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to all resources"
+  default = {
+    Name = "tc3-g38-lanchonete-v1"
+  }
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for the EKS worker nodes"
+  default     = "t3.medium"
+}
